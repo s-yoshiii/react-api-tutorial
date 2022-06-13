0588dfeefec52a6f4b9ea59c215fb0e1e8fe7e70
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-06-13 12:49:16 +0900

    APIの取得をaxiosで書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
 import Gallery from "./component/Gallery";
+import axios from "axios";
 import { Fetch } from "./types/api";
+
 const endpointURL =
   "https://pixabay.com/api/?key=27953433-575aa647eb0ffb5a156b64201&image_type=photo&pretty=true&q=";
 
 function App() {
   const [fetchData, setFetchData] = useState<Fetch[]>([]);
   const ref = useRef<HTMLInputElement>(null);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    if (!ref.current?.value) {
-      return;
-    }
-    fetch(`${endpointURL}${ref.current.value}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setFetchData(data.hits);
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>): void => {
+      e.preventDefault();
+      if (!ref.current?.value) {
+        setFetchData([]);
+        return;
+      }
+      axios.get(`${endpointURL}${ref.current.value}`).then((res) => {
+        setFetchData(res.data.hits);
       });
-  };
+    },
+    [ref]
+  );
   return (
     <div className="container">
       <h1>My Pixabay</h1>
